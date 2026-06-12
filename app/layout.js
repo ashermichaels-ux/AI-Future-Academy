caee4aea7d22d5c1112a3d721ed0aeb3938d3eaa
ashermichaels-ux <officialashermichaels@gmail.com>
2026-06-12 21:01:56 +0100

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,9 @@
+export const metadata = { title: 'AI Future Academy Quiz' }
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
